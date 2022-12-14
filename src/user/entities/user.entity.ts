546c842ebefb6a2b36cdb305af2ec89513bb356c
Samuel Haro <samuel.hm@hotmail.com>
2022-12-14 10:15:04 -0300

    created: user entity / adds: bcrypt types

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,31 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+
+@Entity({ name: 'user' })
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 100, nullable: false })
+  fullName: string;
+
+  @Column({ length: 75, unique: true, nullable: false })
+  email: string;
+
+  @Column({ nullable: true })
+  photoUrl: string;
+
+  @Column({ nullable: true })
+  phone: string;
+
+  @Column({ nullable: false })
+  password: string;
+
+  @Column({ nullable: false })
+  salt: string;
+
+  async checkPassword(password: string): Promise<boolean> {
+    const hash = await bcrypt.hash(password, this.salt);
+    return hash === this.password;
+  }
+}
