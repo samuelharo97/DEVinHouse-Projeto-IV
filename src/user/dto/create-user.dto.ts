48ba0282ea3e2d588409f0290e63425887e5a1c4
Samuel Haro <samuel.hm@hotmail.com>
2022-12-14 13:21:24 -0300

    created: create-user-dto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,29 @@
-export class CreateUserDto {}
+export class CreateUserDto {
+  email: string;
+
+  fullName: string;
+
+  password: string;
+
+  confirm_password: string;
+
+  photoUrl?: string;
+
+  phone: string;
+
+  userAddress: {
+    zipCode: string;
+
+    street: string;
+
+    number: number;
+
+    neighborhood: string;
+
+    city: string;
+
+    state: string;
+
+    complement?: string;
+  };
+}
