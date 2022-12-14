7e0a5692a53880b9c1f27c75419a76f4b360ac80
Samuel Haro <samuel.hm@hotmail.com>
2022-12-14 10:42:04 -0300

    adds: user.provider

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: {
+        expiresIn: 60 * 6 * 4,
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { databaseProviders } from 'src/core/database/database.providers';
+import { userProviders } from './user.providers';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService]
+  providers: [...databaseProviders, ...userProviders, UserService],
 })
 export class UserModule {}
